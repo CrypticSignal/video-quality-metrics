a02f3cbfff9994976e5d00dbb48e8b9c4ddc0fd1
BassThatHertz <48166845+BassThatHertz@users.noreply.github.com>
2020-11-05 15:51:16 +0000

    Added functions for getting the framerate.

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 from ffmpeg import probe
 import math
 
-def GetBitrate(videofile):
-	videofileprobe = probe(videofile)
-	format = videofileprobe['format']
-	bitrate = format['bit_rate']
-	return f'{math.trunc(int(bitrate) / 1000)} kbits/s'
+def get_framerate_fraction(video_path):
+    r_frame_rate = [stream for stream in probe(video_path)['streams'] if stream['codec_type'] == 'video'][0]['r_frame_rate']
+    return r_frame_rate
+
+
+def get_framerate_float(video_path):
+    numerator, denominator = get_framerate_fraction(video_path).split('/')
+    return round((int(numerator) / int(denominator)), 3)
+
+
+def get_bitrate(video_path):
+    bitrate = probe(video_path)['format']['bit_rate']
+    return f'{math.trunc(int(bitrate) / 1000)} kbit/s'
