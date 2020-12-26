b6831374130a489b22eafac3a4db3bb839991216
BassThatHertz <hshafiq@hotmail.co.uk>
2020-12-26 00:37:03 +0000

    Condensed the contents of a function.

--- arguments_validator.py
+++ arguments_validator.py
@@ -25,12 +25,10 @@ class ArgumentsValidator:
         return (os.path.exists(video_path), f'Unable to find {video_path}')
 
     def __validate_crf_and_preset_count(self, no_transcoding_mode, crf_values, presets):
-        result = False
         if not no_transcoding_mode and isinstance(crf_values, int) and isinstance(presets, str):
-            return (result, 'No CRF value or preset has been specified. Did you mean to use the -ntm mode?')
+            return (False, 'No CRF value or preset has been specified. Did you mean to use the -ntm mode?')
 
         elif is_list(crf_values) and len(crf_values) > 1 and is_list(presets) and len(presets) > 1:
-            return (result, 'More than one CRF value AND more than one preset specified. No suitable mode found.')
-
-        else:
-            return (True, '')
+            return (False, 'More than one CRF value AND more than one preset specified. No suitable mode found.')
+            
+        return (True, '')
