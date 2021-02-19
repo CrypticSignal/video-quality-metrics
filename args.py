156116a515a689a6c5cc722befd6ccd60bcb988c
CrypticSignal <hshafiq@hotmail.co.uk>
2021-02-19 01:43:35 +0000

    Change formatter_class to ArgumentDefaultsHelpFormatter, plus miscellaneous changes.

--- args.py
+++ args.py
@@ -1,7 +1,7 @@
 import os
-from argparse import ArgumentParser, RawTextHelpFormatter
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
 encoding_args = parser.add_argument_group('Encoding Arguments')
 vmaf_args = parser.add_argument_group('VMAF Arguments')
@@ -16,9 +16,8 @@ encoding_args.add_argument(
     default=5,
     choices=range(1, 9),
     metavar='<1-8>',
-    help='Only applicable if choosing the AV1 encoder. Set the quality/encoding speed tradeoff.\n'
-         'Lower values mean slower encoding but better quality, and vice-versa.\n'
-         'If this argument is not specified, the value will be set to 5.'
+    help='Only applicable if the libaom-av1 (AV1) encoder is chosen. Set the quality/encoding speed tradeoff. '
+         'Lower values mean slower encoding but better quality, and vice-versa'
 )
 
 # The length of each clip for Overview Mode.
@@ -30,7 +29,7 @@ overview_mode_args.add_argument(
     metavar='<1-60>',
     help='When using Overview Mode, a X seconds long segment is taken from the original video every --interval seconds '
          'and these segments are concatenated to create the overview video. '
-         'Specify a value for X (in the range 1-60).'
+         'Specify a value for X (in the range 1-60)'
 )
 
 # CRF value(s).
@@ -41,7 +40,7 @@ encoding_args.add_argument(
     choices=range(0, 52),
     nargs='+',
     metavar='<0-51>',
-    help='Specify the CRF value(s) to use.', 
+    help='Specify the CRF value(s) to use', 
 )
 
 # Number of decimal places to use for the data.
@@ -49,7 +48,7 @@ general_args.add_argument(
     '-dp', '--decimal-places', 
     type=int,
     default=2, 
-    help='The number of decimal places to use for the data in the table (default: 2).\nExample: -dp 3'
+    help='The number of decimal places to use for the data in the table'
 )
 
 # Video Encoder
@@ -57,8 +56,8 @@ encoding_args.add_argument(
     '-e', '--video-encoder', 
     type=str, 
     default='x264', 
-    choices=['x264', 'x265', 'av1'],
-    help='Specify the encoder to use (default: x264).'
+    choices=['x264', 'x265', 'libaom-av1'],
+    help='Specify whether to use the x264 (H.264), x265 (H.265) or libaom-av1 (AV1) encoder'
 )
 
 # The time interval for Overview Mode.
@@ -70,7 +69,7 @@ overview_mode_args.add_argument(
     metavar='<1-600>',
     help='To activate Overview Mode, this argument must be specified. '
          'Overview Mode creates a lossless overview video by grabbing a --clip-length long segment every X seconds '
-         'from the original video. Specify a value for X (in the range 1-600).'
+         'from the original video. Specify a value for X (in the range 1-600)'
 )
 
 # n_subsample
@@ -80,7 +79,7 @@ vmaf_args.add_argument(
     default='1',
     #metavar='x',
     help='Set a value for libvmaf\'s n_subsample option if you only want the VMAF/SSIM/PSNR to be calculated for every nth '
-         'frame.\nWithout this argument, VMAF/SSIM/PSNR scores will be calculated for every frame.\nExample: -n 24'
+         'frame. Without this argument, VMAF/SSIM/PSNR scores will be calculated for every frame'
 )
 
 # Set the number of threads to be used when computing VMAF.
@@ -88,10 +87,7 @@ vmaf_args.add_argument(
     '--n-threads',
     type=str,
     default=str(os.cpu_count()),
-    help='Set the number of threads to be used when computing VMAF.\n'
-         'The default is set to what Python\'s os.cpu_count() method returns. '
-         'For example, on a dual-core Intel CPU with hyperthreading, the default will be set to 4.\n'
-         'Example: --n-threads 2'
+    help='Specify the number of threads to use when calculating VMAF'
 )
 
 # -ntm mode
@@ -99,17 +95,16 @@ general_args.add_argument(
     '-ntm', '--no-transcoding-mode', 
     action='store_true',
     help='Enable "no transcoding mode", which allows you to '
-         'calculate the VMAF/SSIM/PSNR for a video that you have already transcoded.\n'
-         'The original and transcoded video paths must be specified using the -ovp and -tvp arguments, respectively.\n'
-         'Example: python main.py -ntm -ovp original.mp4 -tvp transcoded.mp4 -ssim'
+         'calculate the VMAF/SSIM/PSNR for a video that you have already transcoded. '
+         'The original and transcoded video paths must be specified using the -ovp and -tvp arguments, respectively. '
+         'Example: python main.py -ntm -ovp original.mp4 -tvp transcoded.mp4'
 )
 
 general_args.add_argument(
     '-o', '--output-folder',
     type=str,
-    help='Use this argument if you want a specific name for the output folder.\n'
-         'If you want the name of the output folder to contain a space, the string must be surrounded in double quotes.'
-         '\nExample: -o "VQM Output"'
+    help='Use this argument if you want a specific name for the output folder. '
+         'If you want the name of the output folder to contain a space, the string must be surrounded in double quotes'
 )
 
 # Original Video Path
@@ -119,8 +114,7 @@ general_args.add_argument(
     required=True,
     help='Enter the path of the original '
          'video. A relative or absolute path can be specified. '
-         'If the path contains a space, it must be surrounded in double quotes.\n'
-         'Example: -ovp "C:/Users/H/Desktop/file 1.mp4"'
+         'If the path contains a space, it must be surrounded in double quotes'
 )
 
 # Preset(s).
@@ -133,21 +127,21 @@ encoding_args.add_argument(
     ],
     nargs='+', 
     metavar='<preset/s>',
-    help='Specify the preset(s) to use.'
+    help='Specify the preset(s) to use'
 )
 
 # Phone Model
 vmaf_args.add_argument(
     '--phone-model',
     action='store_true', 
-    help='Enable VMAF phone model.'
+    help='Enable VMAF phone model'
 )
 
 # PSNR
 optional_metrics_args.add_argument(
     '-psnr', '--calculate-psnr', 
     action='store_true', 
-    help='Enable PSNR calculation in addition to VMAF (default: disabled).'
+    help='Enable PSNR calculation in addition to VMAF'
 )
 
 # Show the commands being run.
@@ -160,8 +154,8 @@ general_args.add_argument(
 # SSIM
 optional_metrics_args.add_argument(
     '-ssim', '--calculate-ssim', 
-    action='store_true', 
-    help='Enable SSIM calculation in addition to VMAF (default: disabled).'
+    action='store_true',
+    help='Enable SSIM calculation in addition to VMAF'
 )
 
 # Use only the first x seconds of the original video.
@@ -171,19 +165,19 @@ general_args.add_argument(
     metavar='SECONDS',
     help='Create a lossless version of the original video that is just the first x seconds of the video. '
          'This cut version of the original video is what will be transcoded and used as the reference video. '
-         'You cannot use this option in conjunction with the -i or -cl arguments.\nExample: -t 60'
+         'You cannot use this option in conjunction with the -i or -cl arguments'
 )
 
 # Transcoded video path (only applicable when using the -ntm mode).
 general_args.add_argument(
     '-tvp', '--transcoded-video-path',
-    help='The path of the transcoded video (only applicable when using the -ntm mode).'
+    help='The path of the transcoded video (only applicable when using the -ntm mode)'
 )
 
 # FFmpeg Video Filter(s)
 general_args.add_argument(
     '-vf', '--video-filters',
     type=str,
-    help='Add FFmpeg video filter(s). Each filter must be separated by a comma.\n'
+    help='Add FFmpeg video filter(s). Each filter must be separated by a comma. '
          'Example: -vf bwdif=mode=0,crop=1920:800:0:140'
 )
