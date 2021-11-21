f450e42011f105ee6840a35ce6d87a8313c0b1a9
CrypticSignal <hshafiq@hotmail.co.uk>
2021-11-21 19:33:08 +0000

    Misc.

--- libvmaf.py
+++ libvmaf.py
@@ -1,18 +1,26 @@
 from ffmpeg_process_factory import LibVmafArguments
-from utils import Logger
+from utils import line, Logger
 
-log = Logger('libvmaf')
+log = Logger("libvmaf")
 
 # Change this if you want to use a different VMAF model file.
-vmaf_model_file_path = 'vmaf_models/vmaf_v0.6.1.json'
+vmaf_model_file_path = "vmaf_models/vmaf_v0.6.1.json"
 
 
-def run_libvmaf(transcode_output_path, args, json_file_path, fps, original_video_path, factory, duration, 
-                crf_or_preset=None):
+def run_libvmaf(
+    transcode_output_path,
+    args,
+    json_file_path,
+    fps,
+    original_video_path,
+    factory,
+    duration,
+    crf_or_preset=None,
+):
     characters_to_escape = ["'", ":", ",", "[", "]"]
     for character in characters_to_escape:
         if character in json_file_path:
-            json_file_path = json_file_path.replace(character, f'\{character}')    
+            json_file_path = json_file_path.replace(character, f"\{character}")
 
     vmaf_options = {
         "log_fmt": "json",
@@ -22,31 +30,35 @@ def run_libvmaf(transcode_output_path, args, json_file_path, fps, original_video
         "phone_model": "1" if args.phone_model else "0",
         "psnr": "1" if args.calculate_psnr else "0",
         "ssim": "1" if args.calculate_ssim else "0",
-        "n_threads": args.n_threads
+        "n_threads": args.n_threads,
     }
-    vmaf_options = ":".join(f'{key}={value}' for key, value in vmaf_options.items())
+    vmaf_options = ":".join(f"{key}={value}" for key, value in vmaf_options.items())
 
-    libvmaf_arguments = LibVmafArguments(fps, transcode_output_path, original_video_path, vmaf_options)
+    libvmaf_arguments = LibVmafArguments(
+        fps, transcode_output_path, original_video_path, vmaf_options
+    )
     video_filters = args.video_filters if args.video_filters else None
     libvmaf_arguments.video_filters(video_filters)
 
     process = factory.create_process(libvmaf_arguments, args)
 
     if args.calculate_psnr and args.calculate_ssim:
-        end_of_computing_message = ', PSNR and SSIM'
+        end_of_computing_message = ", PSNR and SSIM"
     elif args.calculate_psnr:
-        end_of_computing_message = ' and PSNR'
+        end_of_computing_message = " and PSNR"
     elif args.calculate_ssim:
-        end_of_computing_message = ' and SSIM'
+        end_of_computing_message = " and SSIM"
     else:
-        end_of_computing_message = ''
+        end_of_computing_message = ""
 
     if not args.no_transcoding_mode:
         if isinstance(args.crf, list) and len(args.crf) > 1:
-            end_of_computing_message += f' achieved with CRF {crf_or_preset}'
+            end_of_computing_message += f" achieved with CRF {crf_or_preset}"
         else:
-            end_of_computing_message += f' achieved with preset {crf_or_preset}'
+            end_of_computing_message += f" achieved with preset {crf_or_preset}"
 
-    print(f'Calculating the VMAF{end_of_computing_message}...')
-    process.run(duration)
-    log.info('Done!')
+    line()
+    print(f"Calculating the VMAF{end_of_computing_message}...")
+
+    process.run(original_video_path, duration)
+    log.info("Done!")
